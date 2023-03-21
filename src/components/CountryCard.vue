d88bdc5d4cf012cd7905d3c3d2cbee3f950302b3
odewitness <audepora82@orange.fr>
2023-03-21 15:37:57 +0100

    ✨ Ajout fonctions de tri (JS)

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="select-container">
     <i-select v-model="selected" :options="options" autocomplete placeholder="Choose something.." @search="onSearch"
-      clearable />
+      clearable @update:model-value="onSelectOption"><template #prepend>
+        <span>Je trie...</span>
+      </template></i-select>
   </div>
 
   <div class="container">
@@ -25,7 +27,7 @@
         </span>
       </p>
       <p>
-        <strong>Population :</strong> {{ country.population }} habitants
+        <strong>Population :</strong> {{ country.population.toLocaleString('fr-FR') }} habitants
       </p>
       <i-button color="primary">Voir plus</i-button>
     </i-card>
@@ -42,10 +44,10 @@ export default {
   },
   data() {
     const defaultOptions = [
-      { id: 1, label: 'Nom pays par ordre croissant' },
-      { id: 2, label: 'Nom pays par ordre décroissant' },
-      { id: 3, label: "Nombre d'habitants par ordre croissant" },
-      { id: 4, label: "Nombre d'habitants par ordre décroissant" }
+      { id: 1, label: 'les noms des pays par ordre croissant' },
+      { id: 2, label: 'les noms des pays par ordre décroissant' },
+      { id: 3, label: "le nombre d'habitants par ordre croissant" },
+      { id: 4, label: "le nombre d'habitants par ordre décroissant" }
     ];
 
     return {
@@ -61,12 +63,40 @@ export default {
         .filter((option) => {
           return option.label.toLowerCase().includes((query || '').toLowerCase());
         });
+    },
+    sortByNameDesc() {
+      this.countries.sort((a, b) => b.name.localeCompare(a.name));
+    },
+    sortByNameAsc() {
+      this.countries.sort((a, b) => a.name.localeCompare(b.name));
+    },
+    sortByPopulationDesc() {
+      this.countries.sort((a, b) => b.population - a.population);
+    },
+    sortByPopulationAsc() {
+      this.countries.sort((a, b) => a.population - b.population);
+    },
+    onSelectOption(option) {
+      if (option.id == 1) {
+        this.sortByNameAsc();
+      }
+      if (option.id == 2) {
+        this.sortByNameDesc();
+      }
+      if (option.id == 3) {
+        this.sortByPopulationAsc();
+      }
+      if (option.id == 4) {
+        this.sortByPopulationDesc();
+      }
+      // this.countries = [...this.countries];
     }
   },
   mounted() {
     getAllCountries()
       .then(data => {
         this.countries = data;
+        // this.sortByNameDesc();
       })
       .catch(error => console.error(error));
   }
@@ -100,16 +130,16 @@ export default {
 }
 
 .select-container {
-  display: flex;
+  /* display: flex; */
   justify-content: center;
   align-items: center;
   margin: 0 auto;
-  max-width: 50%;
+  max-width: 83%;
   padding-top: 2%;
   padding-bottom: 1%;
 }
 
 .i-select {
-  width: 50%;
+  /* width: 80%; */
 }
 </style>

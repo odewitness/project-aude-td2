743ac9a556c832d9834974a52512704523e6347b
odewitness <audepora82@orange.fr>
2023-05-16 13:48:53 +0200

    Push dist

--- src/services/api/countriesAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export function getAllCountries() {
-    return axios.get('https://restcountries.com/v2/all')
-        .then(response => response.data)
-        .catch(error => console.error(error));
-}
